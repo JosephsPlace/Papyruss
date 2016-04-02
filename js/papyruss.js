7449ca24e623a408f115047a76c7e4bb24949fea
Joseph Gengarella <jag0114@cox.net>
2016-04-01 23:57:19 -0500

    Breaking then fixing responsive nav bars. Still a little weird

--- js/papyruss.js
+++ js/papyruss.js
@@ -23,16 +23,49 @@ if (typeof jQuery == 'undefined') {
 ~function ($) {
    'use strict';
 
+   var window_width = $(window).width();
+   var page_content = $('#page-content');
+   var sidebar = $('.nav-sidebar');
+   var nav_toggle_link = $('.toggle-wrapper');
    var nav_toggle = $('.nav-toggle');
    var navbar = $('#navbar');
 
-   if (nav_toggle) {
-      navbar.hide();
-   }
+    $(window).resize(function ( e ) {
+       var window_width = $(this).width();
+    });
 
-   nav_toggle.on('click', function ( e ) {
-      $(this).toggleClass('opened');
-      navbar.slideToggle();
+   if (sidebar.length)
+      navbar.show();
+
+   $(window).resize(function ( e ) {
+      if ($(this).width() > 767) {
+         if (sidebar.length)
+            sidebar.css({'margin-left' : 0});
+         if (!sidebar.length)
+            navbar.show();
+      }
+      console.log($(this).width());
+   });
+
+   nav_toggle_link.on('click', function ( e ) {
+      e.preventDefault();
+      $(nav_toggle).toggleClass('opened');
+      if (!sidebar.length) {
+         console.log("test");
+         navbar.slideToggle();
+      } else if (sidebar.length) {
+         console.log("test2");
+         sidebar.toggleClass('sidebar-active');
+         if (sidebar.hasClass('sidebar-active')) {
+            sidebar.animate({
+               'margin-left' : 0
+            });
+         } else {
+            sidebar.animate({
+               'margin-left' : -250
+            });
+         }
+      }
    });
 }(jQuery);
 
@@ -42,11 +75,14 @@ if (typeof jQuery == 'undefined') {
 ~function ($) {
    'use strict';
 
+   var nav_opened = $('.opened');
+   var page_content = $('#page-content');
+   var has_sidebar = $('nav-sidebar');
    var is_fixed = $('.nav-top-fixed').length;
 
    if (is_fixed)
-      $('body').css({
-         'padding-top' : $('nav').height() + 20
+      page_content.css({
+         'margin-top' : $('nav').height() + 20
       });
 
 
