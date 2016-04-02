7675d95f31067282e07827c14e9f71bebc3652f6
Joseph Gengarella <jag0114@cox.net>
2016-04-02 00:40:17 -0500

    Fixed nav responsiveness. Now passes all tests correctly

--- js/papyruss.js
+++ js/papyruss.js
@@ -5,6 +5,9 @@
  *
 */
 
+/*
+ * Make sure jQuery is included
+*/
 if (typeof jQuery == 'undefined') {
    throw new Error('Papyruss needs jQuery to run correcly, sorry :(');
 }
@@ -12,19 +15,20 @@ if (typeof jQuery == 'undefined') {
 
 /*
  * This is the basic function structure
+ *
+ * ~function ($) {
+ *    'use strict';
+ * }(jQuery);
 */
-~function ($) {
-   'use strict';
-}(jQuery);
 
 /*
- * Navbar toggle
+ * All logic behind the navbar toggle and it's responsiveness
 */
 ~function ($) {
    'use strict';
 
    var window = $(window);
-   var prev_size = $(window).width();
+   var prev_size = window.width();
    var sidebar = $('.nav-sidebar');
    var nav_toggle_link = $('.toggle-wrapper');
    var nav_toggle = $('.nav-toggle');
@@ -34,21 +38,32 @@ if (typeof jQuery == 'undefined') {
       navbar.show();
    }
 
-   $(window).resize(function ( e ) {
-      if ($(this).width() > 767) {
+   window.resize(function ( e ) {
+      e.preventDefault();
+
+      if ($(this).width() > 767 && prev_size < 767) {
             if (sidebar.length) {
                sidebar.css({'margin-left' : 0});
+               nav_toggle.removeClass('opened');
+               sidebar.removeClass('sidebar-active');
             }
-            if (!sidebar.length)
+            if (!sidebar.length) {
                navbar.show();
+            }
             prev_size = $(this).width();
-      } else if ($(this).width() < 768) {
+         prev_size = $(this).width();
+      } else if ($(this).width() < 768 && prev_size > 768) {
             if (sidebar.length) {
                navbar.show();
                sidebar.css({
                   'margin-left' : -250
                });
             }
+            if (!sidebar.length) {
+               nav_toggle.removeClass('opened');
+               navbar.hide();
+            }
+         prev_size = $(this).width();
       }
    });
 
@@ -77,20 +92,19 @@ if (typeof jQuery == 'undefined') {
 }(jQuery);
 
 /*
- * Fixed navbar
+ * Adds padding on top of screen for fixed top nav bar
 */
 ~function ($) {
    'use strict';
 
-   var nav_opened = $('.opened');
    var page_content = $('#page-content');
-   var has_sidebar = $('nav-sidebar');
    var is_fixed = $('.nav-top-fixed').length;
 
-   if (is_fixed)
+   if (is_fixed) {
       page_content.css({
          'margin-top' : $('nav').height() + 20
       });
+   }
 
 
 }(jQuery);
